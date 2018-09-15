b448d756c21eb976ee54b81483bbfcd165c432cd
Heaven <yorie@mail.ru>
2018-09-15 18:47:53 +0300

    Update ArcherTower.cs

--- WpfTowerDefence/Towers/ArcherTower.cs
+++ WpfTowerDefence/Towers/ArcherTower.cs
@@ -6,7 +6,7 @@ namespace WpfTowerDefence
     class ArcherTower : Tower
     {
         static SolidColorBrush colorNorm = Brushes.SkyBlue;
-        public static int price = 10;
+        public static int price = 10; // TODO: Not a constant value, could be changed from outside, but will not take effect on created entities. Make it const or private, or better pass as entity configuration.
         public ArcherTower(Cell location, Canvas canvasMap, Player player) : base(location, canvasMap, colorNorm, price, player)
         {
             this.range = 3;
